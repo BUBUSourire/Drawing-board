a64a93c413541ab2466fd63e47d9a1673c35baf1
Sourire <953810390@qq.com>
2019-06-25 21:20:44 +0800

    1

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ function my$(id) {
 
 function draw(ev) {
   var canvas = my$("canvas");
+  var lineWidth=2;
   if (canvas.getContext) {
     var context = canvas.getContext("2d");
 
@@ -25,7 +26,7 @@ function draw(ev) {
     function drawLine(x1, y1, x2, y2) {
       context.beginPath();
       context.moveTo(x1, y1);//起点
-      context.lineWidth = 2;//线条粗细
+      context.lineWidth = lineWidth;//线条粗细
       context.lineTo(x2, y2);//终点
       context.stroke();
       context.closePath();
@@ -100,17 +101,86 @@ function draw(ev) {
         using = false;
       };
     }
-    //为橡皮擦注册点击事件=========================
+
     var eraserEnable = false;
-    my$("eraser").onclick = function () {
-      eraserEnable = !eraserEnable;
-    };
     //为画笔注册点击事件
     my$("brush").onclick = function () {
       eraserEnable = false;
-    }
-  }
+      my$("brush").classList.add('active');
+      my$("eraser").classList.remove('active');
+      my$("clear").classList.remove('active');
 
+    }
+    //为橡皮擦注册点击事件=========================
+    my$("eraser").onclick = function () {
+      eraserEnable = !eraserEnable;
+      my$("eraser").classList.add('active');
+      my$("brush").classList.remove('active');
+      my$("clear").classList.remove('active');
+    };
+
+    //注册清除事件
+    my$("clear").onclick = function () {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      my$("clear").classList.add('active');
+      my$("brush").classList.remove('active');
+      my$("eraser").classList.remove('active');
+    };
+
+    //注册下载事件
+    my$("download").onclick=function(){
+      var url =canvas.toDataURL("img/png");
+      var a =document.createElement("a");
+      document.body.appendChild(a);
+      a.href=url;
+      a.download="图片下载";
+      a.target="_blank";
+      a.click();
+    }
+
+    //注册线条粗细
+    my$("thin").onclick=function(){
+      lineWidth=2;
+    };
+    my$("fat").onclick=function(){
+      lineWidth=4;
+    };
+
+    //注册颜色点击事件
+    my$("red").onclick = function () {
+      context.fillStyle = "red";
+      context.strokeStyle = "red";
+      my$("red").classList.add('active');
+      my$("green").classList.remove('active');
+      my$("yellow").classList.remove('active');
+      my$("black").classList.remove('active');
+    };
+    my$("green").onclick = function () {
+      context.fillStyle = "green";
+      context.strokeStyle = "green";
+      my$("green").classList.add('active');
+      my$("red").classList.remove('active');
+      my$("yellow").classList.remove('active');
+      my$("black").classList.remove('active');
+    };
+    my$("yellow").onclick = function () {
+      context.fillStyle = "yellow";
+      context.strokeStyle = "yellow";
+      my$("yellow").classList.add('active');
+      my$("green").classList.remove('active');
+      my$("red").classList.remove('active');
+      my$("black").classList.remove('active');
+    };
+    my$("black").onclick = function () {
+      context.fillStyle = "black";
+      context.strokeStyle = "black";
+      my$("black").classList.add('active');
+      my$("green").classList.remove('active');
+      my$("red").classList.remove('active');
+      my$("yellow").classList.remove('active');
+    };
+  
+  }
 };
 
 
